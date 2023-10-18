d8c2517378559c3ca7d1c2f832f6c468b5a686a9
Adhney <adhney@metadome.ai>
2023-10-18 11:51:05 +0530

    changed tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,42 @@
+// const { nextui } = require("@nextui-org/react");
+
+// import type { Config } from "tailwindcss";
+// import typography from "@tailwindcss/typography";
+
+// export default {
+//   content: [
+//     "./app/**/*.{ts,tsx}",
+//     "./components/**/*.{ts,tsx}",
+//     "./content/**/*.mdx",
+//     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["var(--font-graphik)"],
+//       },
+//       typography: {
+//         quoteless: {
+//           css: {
+//             "blockquote p:first-of-type::before": { content: "none" },
+//             "blockquote p:first-of-type::after": { content: "none" },
+//           },
+//         },
+//       },
+//     },
+//   },
+//   future: {
+//     hoverOnlyWhenSupported: true,
+//   },
+//   darkMode: "class",
+//   plugins: [typography, nextui()],
+// } satisfies Config;
+
+// tailwind.config.js
 const { nextui } = require("@nextui-org/react");
 
-import type { Config } from "tailwindcss";
-import typography from "@tailwindcss/typography";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./app/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -11,23 +44,8 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-graphik)"],
-      },
-      typography: {
-        quoteless: {
-          css: {
-            "blockquote p:first-of-type::before": { content: "none" },
-            "blockquote p:first-of-type::after": { content: "none" },
-          },
-        },
-      },
-    },
-  },
-  future: {
-    hoverOnlyWhenSupported: true,
+    extend: {},
   },
   darkMode: "class",
-  plugins: [typography, nextui()],
-} satisfies Config;
+  plugins: [nextui()],
+};
