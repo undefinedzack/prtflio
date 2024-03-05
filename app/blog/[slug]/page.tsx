d987889b072b2a0279ad29c7750fdb0bc9c2a838
undefinedzack <adhney.nawghare@gmail.com>
2024-03-05 22:28:28 +0530

    changed .global.css

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from "next";
-import { notFound } from "next/navigation";
-import { Mdx } from "components/mdx";
-import { allBlogs } from "contentlayer/generated";
-import Balancer from "react-wrap-balancer";
-import ViewCounter from "../view-counter";
-import { getViewsCount } from "lib/metrics";
-import { Suspense } from "react";
+import type { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+import { Mdx } from 'components/mdx';
+import { allBlogs } from 'contentlayer/generated';
+import Balancer from 'react-wrap-balancer';
+// import ViewCounter from '../view-counter';
+import { getViewsCount } from 'lib/metrics';
+import { Suspense } from 'react';
 
 export async function generateMetadata({
   params,
@@ -32,7 +32,7 @@ export async function generateMetadata({
     openGraph: {
       title,
       description,
-      type: "article",
+      type: 'article',
       publishedTime,
       url: `https://adhney.in/blog/${slug}`,
       images: [
@@ -42,7 +42,7 @@ export async function generateMetadata({
       ],
     },
     twitter: {
-      card: "summary_large_image",
+      card: 'summary_large_image',
       title,
       description,
       images: [ogImage],
@@ -58,7 +58,7 @@ function formatDate(date: string) {
   const monthsAgo = currentDate.getMonth() - targetDate.getMonth();
   const daysAgo = currentDate.getDate() - targetDate.getDate();
 
-  let formattedDate = "";
+  let formattedDate = '';
 
   if (yearsAgo > 0) {
     formattedDate = `${yearsAgo}y ago`;
@@ -67,13 +67,13 @@ function formatDate(date: string) {
   } else if (daysAgo > 0) {
     formattedDate = `${daysAgo}d ago`;
   } else {
-    formattedDate = "Today";
+    formattedDate = 'Today';
   }
 
-  const fullDate = targetDate.toLocaleString("en-us", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
+  const fullDate = targetDate.toLocaleString('en-us', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
   });
 
   return `${fullDate} (${formattedDate})`;
@@ -119,5 +119,5 @@ async function Views({ slug }: { slug: string }) {
   } catch (error) {
     console.error(error);
   }
-  return <ViewCounter allViews={views} slug={slug} trackView />;
+  // return <ViewCounter allViews={views} slug={slug} trackView />;
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
 import { allBlogs } from 'contentlayer/generated';
-import ViewCounter from './view-counter';
+// import ViewCounter from './view-counter';
 // import { getViewsCount } from "lib/metrics";
 
 export const metadata: Metadata = {
   title: 'Blog',
   description: 'Read my thoughts on software development, design, and more.',
 };
+console.log(allBlogs, 'HERE');
 
 export default async function BlogPage() {
   // const allViews = await getViewsCount();
-
+  console.log(allBlogs, 'HERE');
   return (
     <section className="mt-5">
       <div className="w-full flex">
@@ -20,12 +21,12 @@ export default async function BlogPage() {
         </p>
       </div>
       {allBlogs
-        .sort((a, b) => {
-          if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
-            return -1;
-          }
-          return 1;
-        })
+        // .sort((a, b) => {
+        //   if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
+        //     return -1;
+        //   }
+        //   return 1;
+        // })
         .map((post) => (
           <Link
             key={post.slug}
